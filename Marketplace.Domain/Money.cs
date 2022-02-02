f29420f768081eb49b3c0478af90f23d6d376718
Dmitry Stepanov <dmitry_stepanov@inbox.ru>
2022-02-03 00:16:28 +0300

    add methods for Money, add Price

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -10,5 +10,25 @@ namespace Marketplace.Domain
         {
             Amount = amount;
         }
+
+        public Money Add(Money money)
+        {
+            return new Money(Amount + money.Amount);
+        }
+
+        public Money Subtract(Money money)
+        {
+            return new Money(Amount - money.Amount);
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            return money1.Add(money2);
+        }
+
+        public static Money operator -(Money money1, Money money2)
+        {
+            return money1.Subtract(money2);
+        }
     }
 }
--- Marketplace.Tests/MoneyTests.cs
+++ Marketplace.Tests/MoneyTests.cs
@@ -13,5 +13,17 @@ namespace Marketplace.Tests
 
             Assert.Equal(money1, money2);
         }
+
+        [Fact]
+        public void Sum_of_money_gives_full_amount()
+        {
+            var coin1 = new Money(1);
+            var coin2 = new Money(2);
+            var coin3 = new Money(2);
+
+            var banknote = new Money(5);
+
+            Assert.Equal(banknote, coin1 + coin2 + coin3);
+        }
     }
 }
